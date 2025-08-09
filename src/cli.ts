82dc9ed421afae33412581a056cb7a0a9d74c4c7
Bill Collins <bill@bill.works>
2025-08-09 12:14:07 +0100

    feat: try reading package.json from cwd

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,36 @@
 #!/usr/bin/env node
 
-import { checkAndPreparePackage } from './index.js'
+import * as fs from 'node:fs'
 
-void checkAndPreparePackage(
-  process.argv[2],
-  process.argv[3],
-  ['1', 'check', 'true', 'yes'].includes(process.argv[4]),
-)
+import { PACKAGE_JSON } from './constants'
+
+import { checkAndPreparePackage, PackageJson } from './index.js'
+
+function readCwdPackage(
+  packageName: string,
+  packageVersion: string,
+): PackageJson | undefined {
+  try {
+    const packageJson = JSON.parse(
+      fs.readFileSync(PACKAGE_JSON, 'utf8'),
+    ) as PackageJson
+    if (
+      packageJson.name === packageName &&
+      packageJson.version === packageVersion
+    ) {
+      return packageJson
+    }
+  } catch {}
+}
+
+const packageName = process.argv[2]
+const packageVersion = process.argv[3]
+const checkVersion = ['1', 'check', 'true', 'yes'].includes(process.argv[4])
+
+const packageJson = readCwdPackage(packageName, packageVersion)
+
+if (packageJson) {
+  void checkAndPreparePackage(packageJson, checkVersion)
+} else {
+  void checkAndPreparePackage(packageName, packageVersion, checkVersion)
+}
